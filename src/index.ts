979bbbb31a7bf78faf5d8944269ba42ae72b3070
ravenna-alencar <ravenna.alencar@gocase.com>
2026-04-01 19:36:51 -0300

    feature first commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-const message = "Hello World!"
+interface Livro {
+  id: number
+  titulo: string
+  autor: string
+  ano: number
+  disponivel: boolean
+}
 
-console.log(message)
+
+const harrypotter: Livro = {
+  id: 1,
+  titulo: "Harry Potter e a Pedra Filosofal",
+  autor: "J.K. Rowling",
+  ano: 1997,
+  disponivel: true
+}
+
+console.log(harrypotter)
